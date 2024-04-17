6f8a112dfb71cca4ae157fd30e5250d02bc46288
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-04-17 10:11:56 +0200

    Syled

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,16 +1,29 @@
 import React from "react"
 import Figure from "./Figure"
+import styled from "styled-components"
+
+const StyledCard = styled.div`
+  border: 1px solid gray;
+  border-radius: 10px;
+  padding: 20px;
+  margin: 10px;
+
+  h2, p {
+    margin-bottom: 20px;
+    text-align: center;
+  }
+`
 
 export default function Card({ title, text, imgUrl, date }) {
     return (
-      <div className='card'>
+      <StyledCard className='card'>
         <h2>{title}</h2>
         <p>{text}</p>
         <Figure 
         imgUrl={imgUrl}
         figDate={date}
         />
-      </div>
+      </StyledCard>
     )
   }
   
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,10 +1,24 @@
 import React from "react"
+import styled from "styled-components"
+
+const StyledFig = styled.figure`
+    max-width: 70%;
+    text-align: center;
+    margin: auto;
+    img {
+        border-radius: 10px;
+    }
+    
+    figcaption {
+        font-style: italic;
+    }
+`
 
 export default function Figure({ imgUrl, figDate }) {
     return (
-        <figure>
+        <StyledFig>
             <img src={imgUrl} />
             <figcaption>Picture taken on {figDate}</figcaption>
-        </figure>
+        </StyledFig>
     )
 }
